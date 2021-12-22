12f3e3eacc4e203030160141f3967b7d99c8dd53
Scyzoryk96 <damianszefler96@gmail.com>
2021-12-22 23:36:57 +0100

    Add some basic functions - UNTESTED

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
 #include <Arduino.h>
+#define MAX_MESSAGE_LENGTH 255
+
 #define LED_PIN PB12
+
+//Definicja funkcji
+char * ReciveSerialMessage();
+void blinkLED(byte numBlinks, int onOffTime);
+//---
+
 void setup() {
-  // put your setup code here, to run once:
+  //Set pin modes
   pinMode(LED_PIN, OUTPUT);
+  //serial init
+  Serial.begin(9600);  //ustawiamy przepustowość łącza szeregowego
+  Serial.setTimeout(10); //ustawiamy czas utracenia połączaniaSERIAL, jest bardzo niski bo nie działamy synchronicznie
+  delay(100);            //migamy ledem na płytce sygnalizując że płytka się uruchomiła
+  Serial.print("Arduino Gotowe\n");
+  delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+ //Check to see if anything is available in the serial receive buffer
+ 
+}
+
+char * ReciveSerialMessage(){
+  while (Serial.available() > 0)
+ {
+   static char message[MAX_MESSAGE_LENGTH];
+   static unsigned int message_pos = 0;
+   char inByte = Serial.read();
+   if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
+   {
+     message[message_pos] = inByte;
+     message_pos++;
+   }
+   else
+   {
+     message[message_pos] = '\0';
+     return message;
+     message_pos = 0;
+   }
+ }
+}
+
+void blinkLED(byte numBlinks, int onOffTime)
+{ //funkcja miganie leda na płytce - do debugowania
+  for (byte n = 0; n < numBlinks; n++)
+  {
+    digitalWrite(LED_PIN, HIGH);
+    delay(onOffTime);
+    digitalWrite(LED_PIN, LOW);
+    delay(onOffTime);
+  }
 }
